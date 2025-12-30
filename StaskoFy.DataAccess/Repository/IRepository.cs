fbd17d87205bfc2a842cf6e798bbdfa52e40a55f
stasko456 <stdimov2007@gmail.com>
2025-12-30 18:56:22 +0200

    Added IRepository and Repository

---
+++ StaskoFy.DataAccess/Repository/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaskoFy.DataAccess.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        IQueryable<T> GetAll();
+        IQueryable<T> Find(Expression<Func<T, bool>> predicate);
+        Task<T> GetByIdAsync(Guid? id);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        Task RemoveAsync(T entity);
+        Task RemoveRangeAsync(IEnumerable<T> entities);
+        Task UpdateAsync(T entity);
+    }
+}
