eab92d157f4675ea212ba6ec076230b8a7004620
stasko456 <stdimov2007@gmail.com>
2026-01-07 14:55:20 +0200

    Added DbContext and Identity

--- StaskoFy.Models/Entities/User.cs
+++ StaskoFy.Models/Entities/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace StaskoFy.Models.Entities
 {
-    public class User //: IdentityUser<Guid>
+    public class User: IdentityUser<Guid>
     {
         [Display(Name = "Profile Picture")]
         public string ImageURL { get; set; } = "/wwwroot/images/defaults/default-user-pfp.png";
