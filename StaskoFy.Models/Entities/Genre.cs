29a141291adf5f9c8116bd69a90a5b31de550915
stasko456 <stdimov2007@gmail.com>
2026-01-07 14:01:05 +0200

    Added models

---
+++ StaskoFy.Models/Entities/Genre.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaskoFy.Models.Entities
+{
+    public class Genre
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 1)]
+        [Display(Name = "Genre's name")]
+        public string Name { get; set; }
+
+        public ICollection<Song> Songs { get; set; } = new List<Song>();
+    }
+}
---
+++ StaskoFy.Models/Entities/PlaylistSong.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaskoFy.Models.Entities
+{
+    public class PlaylistSong
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Playlist))]
+        public Guid PlaylistId { get; set; }
+        public Playlist Playlist { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Song))]
+        public Guid SongId { get; set; }
+        public Song Song { get; set; }
+
+        [Display(Name = "Date added")]
+        public DateOnly DateAdded { get; set; }
+    }
+}
