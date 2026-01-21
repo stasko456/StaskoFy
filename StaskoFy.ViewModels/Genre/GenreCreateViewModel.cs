b2daff67475d9e8209a44716cfe8cf9d69493fcc
stasko456 <stdimov2007@gmail.com>
2026-01-21 20:38:39 +0200

    Seperated The ViewModels into their own Class Library

---
+++ StaskoFy.ViewModels/Genre/GenreCreateViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StaskoFy.ViewModels.Genre
+{
+    public class GenreCreateViewModel
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 1)]
+        [Display(Name = "Genre's name")]
+        public string Name { get; set; }
+    }
+}
---
+++ StaskoFy.ViewModels/User/RegisterViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StaskoFy.ViewModels.User
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Username")]
+        [StringLength(20, MinimumLength = 5)]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "EmailAddress")]
+        [StringLength(60, MinimumLength = 10)]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        [StringLength(20, MinimumLength = 5)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Confirm Password")]
+        [Compare(nameof(Password))]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "Role")]
+        public string Role { get; set; }
+    }
+}
