685f948d8f69905e4a523dd7c153e9eabeed015f
stasko456 <stdimov2007@gmail.com>
2026-01-30 00:20:26 +0200

    Add ImageService

---
+++ StaskoFy.Core/IService/IImageService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaskoFy.Core.IService
+{
+    public interface IImageService
+    {
+        Task<string> UploadImageAsync(IFormFile imageFile, string name);
+    }
+}
---
+++ StaskoFy.Core/Service/ImageService.cs
@@ -0,0 +1,36 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+using StaskoFy.Core.IService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaskoFy.Core.Service
+{
+    public class ImageService : IImageService
+    {
+        private readonly Cloudinary cloudinary;
+
+        public ImageService(Cloudinary _cloudinary)
+        {
+            this.cloudinary = _cloudinary;
+        }
+
+        public async Task<string> UploadImageAsync(IFormFile imageFile, string name)
+        {
+            using var stream = imageFile.OpenReadStream();
+
+            var uploadParams = new ImageUploadParams()
+            {
+                File = new FileDescription(name, stream)
+            };
+
+            var uploadResult = await cloudinary.UploadAsync(uploadParams);
+
+            return uploadResult.SecureUrl.ToString();
+        }
+    }
+}
