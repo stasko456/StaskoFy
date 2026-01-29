8c50438e9ef4989b5772c027d7a6d7f56bc154de
stasko456 <stdimov2007@gmail.com>
2026-01-29 23:39:52 +0200

    Add ArtistService

---
+++ StaskoFy.Core/IService/IArtistService.cs
@@ -0,0 +1,20 @@
+﻿using StaskoFy.ViewModels.Artist;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaskoFy.Core.IService
+{
+    public interface IArtistService
+    {
+        Task<IEnumerable<ArtistViewModel>> GetAllAsync();
+
+        Task<ArtistViewModel?> GetByIdAsync(Guid id);
+
+        Task AddAsync(ArtistViewModel model);
+
+        Task RemoveAsync(Guid id);
+    }
+}
---
+++ StaskoFy.Core/Service/ArtistService.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StaskoFy.Core.IService;
+using StaskoFy.DataAccess.Repository;
+using StaskoFy.Models.Entities;
+using StaskoFy.ViewModels.Artist;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaskoFy.Core.Service
+{
+    public class ArtistService : IArtistService
+    {
+        private readonly IRepository<Artist> artistRepo;
+
+        public ArtistService(IRepository<Artist> _artistRepo)
+        {
+            this.artistRepo = _artistRepo;
+        }
+
+        public async Task<IEnumerable<ArtistViewModel>> GetAllAsync()
+        {
+            return await artistRepo.GetAllAttached()
+                .Select(a => new ArtistViewModel
+                {
+                    Id = a.Id,
+                    UserId = a.UserId
+                }).ToListAsync();
+        }
+
+        public async Task<ArtistViewModel?> GetByIdAsync(Guid id)
+        {
+            var artist = await artistRepo.GetByIdAsync(id);
+
+            return new ArtistViewModel
+            {
+                Id = artist.Id,
+                UserId = artist.UserId
+            };
+        }
+
+        public async Task AddAsync(ArtistViewModel model)
+        {
+            var artist = new Artist
+            {
+                UserId = model.UserId,
+            };
+
+            await artistRepo.AddAsync(artist);
+        }
+
+        public async Task RemoveAsync(Guid id)
+        {
+            var artist = await artistRepo.GetByIdAsync(id);
+
+            await artistRepo.RemoveAsync(artist);
+        }
+    }
+}
