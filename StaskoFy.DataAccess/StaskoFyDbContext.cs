bfe3ef352d45a4224f18d7ab644ceb8ee7ed3b36
stasko456 <stdimov2007@gmail.com>
2026-01-29 23:25:03 +0200

    Add StaskoFyDbContext

---
+++ StaskoFy.DataAccess/StaskoFyDbContext.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using StaskoFy.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaskoFy.DataAccess
+{
+    public class StaskoFyDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
+    {
+        public StaskoFyDbContext(DbContextOptions<StaskoFyDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Album> Albums { get; set; }
+
+        public DbSet<Artist> Artists { get; set; }
+
+        public DbSet<ArtistAlbum> ArtistsAlbums { get; set; }
+
+        public DbSet<ArtistSong> ArtistsSongs { get; set; }
+
+        public DbSet<Genre> Genres { get; set; }
+
+        public DbSet<LikedSongs> LikedSongs { get; set; }
+
+        public DbSet<Playlist> Playlists { get; set; }
+
+        public DbSet<PlaylistSong> PlaylistsSongs { get; set; }
+
+        public DbSet<Song> Songs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.ConfigureWarnings(warnings =>
+            {
+                warnings.Ignore(Microsoft.EntityFrameworkCore.Diagnostics.RelationalEventId.PendingModelChangesWarning);
+            });
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfigurationsFromAssembly(typeof(StaskoFyDbContext).Assembly);
+
+            builder.Entity<User>()
+                .Property(u => u.UserName)
+                .HasMaxLength(20)
+                .IsRequired();
+
+            builder.Entity<User>()
+                .Property(u => u.Email)
+                .HasMaxLength(60)
+                .IsRequired();
+        }
+    }
+}
