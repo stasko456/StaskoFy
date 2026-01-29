d4d0e6488550d101957858b455433c7e971823a8
stasko456 <stdimov2007@gmail.com>
2026-01-29 23:59:10 +0200

    Add viewModels for genres

---
+++ StaskoFy.ViewModels/Genre/GenreCreateViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaskoFy.ViewModels.Genre
+{
+    public class GenreCreateViewModel
+    {
+        [Required(ErrorMessage = "Genre's name must be between 1 and 100 symbols long!")]
+        [MinLength(1)]
+        [MaxLength(100)]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+    }
+}
---
+++ StaskoFy.ViewModels/Genre/GenreEditViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaskoFy.ViewModels.Genre
+{
+    public class GenreEditViewModel
+    {
+        [Required]
+        [Display(Name = "Id")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Genre's name must be between 1 and 100 symbols long!")]
+        [MinLength(1)]
+        [MaxLength(100)]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+    }
+}
