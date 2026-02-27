298dc3ff143c3fae9bb3d45bd640ebf9bc1aea2c
stasko456 <stdimov2007@gmail.com>
2026-02-27 14:21:24 +0200

    Make changes to CloudinarySettings.cs file

--- StaskoFy.Models/Utilities/CloudinarySettings.cs
+++ StaskoFy.Models/Utilities/CloudinarySettings.cs
@@ -8,10 +8,10 @@ namespace StaskoFy.Models.Utilities
 {
     public class CloudinarySettings
     {
-        public string? CloudName { get; set; }
+        public string? CloudName { get; set; } = "";
 
-        public string? ApiKey { get; set; }
+        public string? ApiKey { get; set; } = "";
 
-        public string? ApiSecret { get; set; }
+        public string? ApiSecret { get; set; } = "";
     }
 }
